97f4e0a87e9b0384051b272dc0ab35f5dc49e549
Elizabeth Gulsby <elizabeth.gulsby@gmail.com>
2017-02-09 10:10:43 -0500

    added sql injection handling

--- config/config.js
+++ config/config.js
@@ -1,6 +1,8 @@
-module.exports = {
+var config = {
 	userName: "x1",
 	password: "x1",
 	host: "127.0.0.1",
 	database: "ComparisonApp"
 };
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var config = require('../config/config');
-var mysql      = require('mysql');
+// Include config files that are in gitignore
+var config = require('../config/config.js');
+// Include mysql module to connect to MySQL
+var mysql = require('mysql');
+// Set up a connection to use over and over
 var connection = mysql.createConnection({
   host     : config.host,
   user     : config.userName,
@@ -14,7 +17,73 @@ connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var getImagesQuery = "select * from images";
+
+	//don't want them to be able to select the same image more than once
+	getImagesQuery = "select * from images where id not in" + 
+	"(select ImageID from votes where ip = '"+req.ip+"');"
+	// need an error handler (see if statement before res.render)
+
+	connection.query(getImagesQuery, function(error, results, fields) {
+		// res.json(results); - make sure results from query are displaying
+		// grab a random images from the results
+		var randomIndex = (Math.floor(Math.random() * results.length));
+		// res.json(results[randomIndex]);
+		if (results.length == 0) {
+			res.render('index', { msg: "noImages" });
+		} else {
+			res.render('index', {
+				title: 'Electric or Not',
+				imageToRender: '/images/' + results[randomIndex].imageUrl,
+				imageID: results[randomIndex].id
+			});
+		}
+	})
+});
+
+router.get('/vote/:voteDirection/:imageID', function (req, res, next) {
+	var imageID = req.params.imageID;
+	var voteD = req.params.voteDirection;
+	if (voteD == 'up') {
+		voteD = 1;
+	}
+	else {
+		voteD = -1;
+	}
+	var insertVoteQuery = "INSERT INTO votes (ip, imageId, voteDirection) VALUES ('"+req.ip+"',"+imageID+",'"+voteD+"')";
+	// res.json(insertVoteQuery);
+	// res.json(req.params.voteDirection);
+	connection.query(insertVoteQuery, function(error, results, fields) {
+		if (error) throw error;
+		res.redirect('/');
+ 	})
+});
+
+
+// getting standings page
+router.get('/standings', function(req, res, next) {
+
+});
+
+//sql injection practice route
+router.get('/testQ', function (req, res, next) {
+	// var id1 = [1];
+	// var id2 = [3];
+	// var query = "select * from images where id > ? AND id < ?";
+	// connection.query(query,[id1, id2], function(error, results, fields) {
+	// 	res.json(results);
+	// })
+
+	//running this manually, inserting values upon each refresh
+	var imageIdVoted = 5;
+	var thisvoteDirection = -1;
+	var insertQuery = "insert into votes (ip, ImageID, voteDirection) values(?, ?, ?)";
+	connection.query(insertQuery, [req.ip, imageIdVoted, thisvoteDirection], function (error, results, fields) {
+		var query = "select * from votes";
+		connection.query(query, function (error, results, fields) {
+			res.json(results);
+		});
+	})
 });
 
 module.exports = router;
